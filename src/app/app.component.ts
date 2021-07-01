c5340a1c1ddd20a31f0f338a1fbd92272318a812
Markus Karileet <markuskarileet@hotmail.com>
2021-07-01 16:01:39 +0300

    search UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {IdentifyService} from './services/api/backend-client';
+import {Event} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sille-fe';
+  fileToUpload: any;
+  originalFile?: File;
+  results: Array<string> = [];
+  searching = false;
+  threshold = 0.899;
+
+  constructor(private identifyService: IdentifyService) {
+  }
+
+  handleFileInput(event: any): void {
+    // @ts-ignore
+    const files = event?.target?.files;
+    const fileReader = new FileReader();
+    this.results = [];
+    const item = files.item(0);
+    if (item !== null && item !== undefined) {
+      this.originalFile = item;
+      if (this.originalFile !== undefined) {
+        fileReader.onloadend = e => (this.fileToUpload = fileReader.result);
+        fileReader.readAsDataURL(this.originalFile);
+      }
+    }
+  }
+
+  search(): void {
+    this.searching = true;
+    if (this.fileToUpload) {
+      this.identifyService.findFace(this.threshold, { image: this.fileToUpload}).toPromise().then(paths => {
+        console.log(paths);
+        if (paths.images != null && paths.images.length > 0) {
+          this.results = paths.images;
+        }
+        this.searching = false;
+      }).catch(e => {
+        console.error(e);
+        this.searching = false;
+      });
+    } else {
+      this.searching = false;
+      this.results = [];
+    }
+  }
+  changeThreshold(event: any): void {
+    this.threshold = event.target.value;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ApiModule, Configuration } from './services/api/backend-client';
+import {environment} from '../environments/environment';
+import {HttpClientModule} from '@angular/common/http';
 
+export function apiModuleConfigurationFactory(): Configuration {
+  return new Configuration({
+    basePath: environment.serverUrl
+  });
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule.forRoot(apiModuleConfigurationFactory)
   ],
   providers: [],
   bootstrap: [AppComponent]
